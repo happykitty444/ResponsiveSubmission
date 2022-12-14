7e24f3922a316696182180171206afdeea2f29bb
sdacruz1 <stephanie_da_cruz@brown.edu>
2022-12-13 23:36:08 -0500

    added images, missing iphone hifi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import './App.css';
 
+import WebAIM from "./images/WebAIM-sceenshot.png"
+import VFlea from "./images/VFlea-sceenshot.png"
+import StyleGuide from "./images/StyleGuide.png"
+import Results from "./images/Results-sceenshot.png"
+import LoFiComputer from "./images/LoFi-Computer.png"
+import LoFiIPad from "./images/LoFi-IPad.png"
+import LoFiIPhone from "./images/LoFi-IPhone.png"
+import HiFiDesktop from "./images/HiFi-Desktop.png"
+import HiFiIPad from "./images/HiFi-IPad.png"
+import HiFiIPhone from "./images/HiFi-IPhone.png"
+
 function App() {
   return (
     <div className="App">
@@ -19,13 +30,13 @@ function App() {
         However as an excercise in accessible and responsive design, I sought out a website which I felt had room to grow in these areas. This
         is the website <a href="http://www.vflea.com/">VFlea.com</a>, a virtual flea market website similar in concept to Ebay.
       </p>
-      <img src="" alt="screenshot of Vflea" />
+      <img src={VFlea} alt="screenshot of Vflea" />
       <h3>Accessibility</h3>
       <p>The website WebAIM WAVE runs an algorithmic check to evaluate the accesibility of any particular website. While an algorithm cannot catch
         every possible accesibility concern, this website is a simple, reputable source for a quick overview of a site's accessibilty, and we can
         see it catches some issues already:
       </p>
-      <img src="" alt="screenshot of WebAIM" />
+      <img src={WebAIM} alt="screenshot of WebAIM" />
       <ul>
         <li><b>Errors:</b> Missing alternative text means that screenreaders will have issues describing the website to visually impaired users, and
         is a big barrier to accessibility. 122 of the issues are missing alt text, with the remaining 2 being missing form labels, which are
@@ -67,7 +78,9 @@ function App() {
         impression of the site across devices, and resize seamlessly. The annotations alongside the wireframes serve to identify the core differences made
         from the reference site and the reasoning behind them.
       </p>
-      <img src="" alt="Balsamiq Low Fidelity Wireframes"/>
+      <img src={LoFiComputer} alt="Balsamiq Low Fidelity Wireframes - Desktop"/>
+      <img src={LoFiIPad} alt="Balsamiq Low Fidelity Wireframes - IPad"/>
+      <img src={LoFiIPhone} alt="Balsamiq Low Fidelity Wireframes - IPhone"/>
 
       <h3>Step Two: A Style Guide</h3>
       <p> Once the basic structure of the website is in place, it is time to think about colors, fonts, elements, and the rest of the suite of
@@ -75,7 +88,7 @@ function App() {
         a style guide highlighting the interactive states of the site and maintaining contrast, while also trying to keep true to the original
         color palette of the website.
       </p>
-      <img src="" alt="Figma Style Guide"/>
+      <img src={StyleGuide} alt="Figma Style Guide"/>
 
       <h3>Step Three: High Fidelity Prototype</h3>
       <p> With the structure and the style of the website known, I can put them together to create a detailed mockup of what the website will
@@ -83,7 +96,9 @@ function App() {
         style of the style guide and the structure of the wireframes. It is annotated for use for website developers (in this case, me) to
         understand the construction of this website to be able to achieve this product.
       </p>
-      <img src="" alt="Figma High Fidelity Prototypes"/>
+      <img src={HiFiDesktop} alt="Figma High Fidelity Prototypes - Desktop"/>
+      <img src={HiFiIPad} alt="Balsamiq Low Fidelity Wireframes - IPad"/>
+      <img src={HiFiIPhone} alt="Balsamiq Low Fidelity Wireframes - IPhone"/>
 
       <h2>The Final Product</h2>
       <p>With a detailed high fidelity prototype it is simply a matter of putting in some work to build the website for real. I believe that
@@ -91,9 +106,9 @@ function App() {
         between users and searching for user store pages by location, while also drastically improving the hierarchy and consolidating the 
         elements into a clearer and more carefully guided user experience.
         <br/>
-        <a href="url">Visit the Final Product Here!</a>
+        <a href="https://happykitty444.github.io/ResponsiveRedesign/">Visit the Final Product Here!</a>
       </p>
-      <img src="" alt="Screenshot of Final Website"/>
+      <img src={Results} alt="Screenshot of Final Website"/>
     </div>
   );
 }
